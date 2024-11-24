1a8e9dc0107cfd9ba802709234cadc995685e440
Ben Epstein <ben.epstein97@gmail.com>
2024-11-24 14:35:44 -0500

    align

--- src/features/config.py
+++ src/features/config.py
@@ -12,9 +12,15 @@ from sqlmesh.integrations.github.cicd.config import GithubCICDBotConfig, MergeMe
 
 from features import constants as C
 
-ROOT_DIR = Path(__file__).parent.parent.parent.resolve()
+# We purposefully do NOT resolve this path because it's different per user and the db remembers that, and considers it
+# a breaking change. The relative path fixes that.
+ROOT_DIR = Path(__file__).parent.parent.parent
 DB_FILE = ROOT_DIR / "db.db"
 GOLD_DELTA_PATH = ROOT_DIR / "gold"
+
+RELATIVE_DB_FILE = DB_FILE.relative_to(ROOT_DIR)
+RELATIVE_GOLD_DELTA_PATH = GOLD_DELTA_PATH.relative_to(ROOT_DIR)
+
 
 config = Config(
     # TODO: This connection will be in S3, with the bucket derived by the client name.
@@ -23,8 +29,8 @@ config = Config(
     model_defaults=ModelDefaultsConfig(dialect="duckdb", start="2024-11-02"),
     variables={
         "custom_mult": 5,
-        C.GOLD_DELTA_URI_VAR: str(GOLD_DELTA_PATH),
-        C.DB_URI_VAR: str(DB_FILE),
+        C.GOLD_DELTA_URI_VAR: str(RELATIVE_GOLD_DELTA_PATH),
+        C.DB_URI_VAR: str(RELATIVE_DB_FILE),
     },
     cicd_bot=GithubCICDBotConfig(
         enable_deploy_command=True,  # If True, you can comment /deploy and override the required approver flow
